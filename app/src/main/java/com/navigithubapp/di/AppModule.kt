b226a9872bcea3072942a8478c57d971915138e5
dhruv <dhruv.kaushal11@gmail.com>
2022-07-02 13:28:46 +0530

    Added Hilt DI

---
+++ app/src/main/java/com/navigithubapp/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.example.mvvm_retrofit_imagesearchapp.di
+
+import com.navigithubapp.BuildConfig
+import com.navigithubapp.data.api.ApiInterface
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideUnsplashApi(retrofit: Retrofit) : ApiInterface =
+        retrofit.create(ApiInterface::class.java)
+}
--- app/src/main/java/com/navigithubapp/ui/viewmodel/FetchPullRequestViewModel.kt
+++ app/src/main/java/com/navigithubapp/ui/viewmodel/FetchPullRequestViewModel.kt
@@ -3,28 +3,23 @@ package com.navigithubapp.ui.viewmodel
 import android.app.Application
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.*
-import com.navigithubapp.data.api.ApiClient
 import com.navigithubapp.data.modal.Commit
+import androidx.hilt.lifecycle.ViewModelInject
+
 import com.navigithubapp.data.repository.GithubRepository
 
-
-class FetchPullRequestViewModel(
+class FetchPullRequestViewModel @ViewModelInject constructor(
+    private val netRepo: GithubRepository,
     application: Application
 ) : AndroidViewModel(application) {
 
-    private val netRepo: GithubRepository
     private val progressbarObservable =  MutableLiveData<Boolean>()
     private val pullRequestToFetch = MutableLiveData<List<Commit>>()
     fun observeListGithub(): LiveData<List<Commit>> = pullRequestToFetch
     fun observeLoadingState(): LiveData<Boolean> = progressbarObservable
 
-    init {
-        val apiInterface = ApiClient.getApiInterface(application)
-        netRepo = GithubRepository.getInstance(apiInterface)
 
-    }
-
-    val fetchDataCallBack = object : GithubRepository.NetRepositoryCallback<List<Commit>> {
+     val fetchDataCallBack = object : GithubRepository.NetRepositoryCallback<List<Commit>> {
         override fun onSuccess(data: List<Commit>) {
             pullRequestToFetch.postValue(data)
             progressbarObservable.value = false
@@ -39,26 +34,10 @@ class FetchPullRequestViewModel(
         }
     }
 
-    fun fetchPullRequest(owner : String, repo :String): LiveData<List<Commit>> {
+     fun fetchPullRequest(owner : String, repo :String): LiveData<List<Commit>> {
         progressbarObservable.value = true
         netRepo.getClosedPullRequest(owner,repo, fetchDataCallBack)
         return pullRequestToFetch;
     }
 
-    companion object {
-        private class ViewModelFactory(
-            private val application: Application
-        ) : ViewModelProvider.NewInstanceFactory() {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return FetchPullRequestViewModel(application) as T
-            }
-        }
-
-        fun factory(activity: FragmentActivity): FetchPullRequestViewModel {
-            return ViewModelProviders.of(
-                activity,
-                ViewModelFactory(activity.application)
-            ).get(FetchPullRequestViewModel::class.java)
-        }
-    }
 }
